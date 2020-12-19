e9645e961e41fca69ee8cd5842e3eb29ede31c32
tdargent1 <tdargent1@gmail.com>
2020-12-19 13:46:25 +0100

    Liaison Product & ProductCategory

--- app/src/Entity/ProductCategory.php
+++ app/src/Entity/ProductCategory.php
@@ -3,6 +3,8 @@
 namespace App\Entity;
 
 use App\Repository\ProductCategoryRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
@@ -22,6 +24,16 @@ class ProductCategory
      */
     private $name;
 
+    /**
+     * @ORM\OneToMany(targetEntity=Product::class, mappedBy="productCategory")
+     */
+    private $products;
+
+    public function __construct()
+    {
+        $this->products = new ArrayCollection();
+    }
+
     public function getId(): ?int
     {
         return $this->id;
@@ -35,6 +47,37 @@ class ProductCategory
     public function setName(string $name): self
     {
         $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|Product[]
+     */
+    public function getProducts(): Collection
+    {
+        return $this->products;
+    }
+
+    public function addProduct(Product $product): self
+    {
+        if (!$this->products->contains($product)) {
+            $this->products[] = $product;
+            $product->setProductCategory($this);
+        }
+
+        return $this;
+    }
+
+    public function removeProduct(Product $product): self
+    {
+        if ($this->products->contains($product)) {
+            $this->products->removeElement($product);
+            // set the owning side to null (unless already changed)
+            if ($product->getProductCategory() === $this) {
+                $product->setProductCategory(null);
+            }
+        }
 
         return $this;
     }
