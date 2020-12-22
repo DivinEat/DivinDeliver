9efff7e16c5d25f2900db06ad0b9d101643cd305
tdargent1 <tdargent1@gmail.com>
2020-12-22 19:48:16 +0100

    ADD Entity Menu

---
+++ app/src/Entity/Menu.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\MenuRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=MenuRepository::class)
+ */
+class Menu
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\ManyToOne(targetEntity=Product::class)
+     */
+    private $entree;
+
+    /**
+     * @ORM\ManyToOne(targetEntity=Product::class)
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $main;
+
+    /**
+     * @ORM\ManyToOne(targetEntity=Product::class)
+     */
+    private $dessert;
+
+    /**
+     * @ORM\ManyToOne(targetEntity=Product::class)
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $drink;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getEntree(): ?Product
+    {
+        return $this->entree;
+    }
+
+    public function setEntree(?Product $entree): self
+    {
+        $this->entree = $entree;
+
+        return $this;
+    }
+
+    public function getMain(): ?Product
+    {
+        return $this->main;
+    }
+
+    public function setMain(?Product $main): self
+    {
+        $this->main = $main;
+
+        return $this;
+    }
+
+    public function getDessert(): ?Product
+    {
+        return $this->dessert;
+    }
+
+    public function setDessert(?Product $dessert): self
+    {
+        $this->dessert = $dessert;
+
+        return $this;
+    }
+
+    public function getDrink(): ?Product
+    {
+        return $this->drink;
+    }
+
+    public function setDrink(?Product $drink): self
+    {
+        $this->drink = $drink;
+
+        return $this;
+    }
+}
