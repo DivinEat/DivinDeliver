9a2750b3e0af1e7db9d5d2e26c4fb6f466c3933e
tdargent1 <tdargent1@gmail.com>
2021-02-01 19:38:04 +0100

    ADD User profile : Show & Edit (name, email) - Pwd in progress..

---
+++ app/src/Controller/Back/ProfileController.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace App\Controller\Back;
+
+use App\Form\ProfileType;
+use App\Form\ResetPasswordType;
+use App\Repository\UserRepository;
+use Symfony\Component\Form\FormError;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
+/**
+ * @Route("/profile", name="profile_")
+ */
+class ProfileController extends AbstractController
+{
+    private $passwordEncoder;
+
+    public function __construct(UserPasswordEncoderInterface $passwordEncoder)
+    {
+        $this->passwordEncoder = $passwordEncoder;        
+    }
+
+    /**
+     * @Route("/", name="index", methods={"GET"})
+     */
+    public function index(UserRepository $userRepository): Response
+    {
+        return $this->render('back/profile/index.html.twig');
+    }
+
+    /**
+     * @Route("/edit", name="edit", methods={"GET","POST"})
+     */
+    public function edit(Request $request): Response
+    {
+        $user = $this->getUser();
+
+        $form = $this->createForm(ProfileType::class, $user);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->getDoctrine()->getManager()->flush();
+
+            return $this->redirectToRoute('admin_profile_index');
+        }
+
+        return $this->render('back/profile/edit.html.twig', [
+            'form' => $form->createView()
+        ]);
+    }
+
+    /**
+     * @Route("/edit/password", name="edit-password", methods={"GET","POST"})
+     */
+    public function editPassword(Request $request)
+    {
+    	$em = $this->getDoctrine()->getManager();
+        $user = $this->getUser();
+
+        $oldPwd = $this->getUser()->getPassword();
+
+    	$form = $this->createForm(ResetPasswordType::class, $user);
+
+    	$form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $oldPassword = $request->request->get('reset_password')['oldPassword'];
+
+            // dump($oldPassword);
+            // dump($oldPwd);
+            // dump($user->getPassword());
+            // dump($this->passwordEncoder->isPasswordValid($user, $oldPassword));
+            // die();
+
+            // $this->passwordEncoder->isPasswordValid($user, $oldPassword)
+            if ($oldPwd === $oldPassword) {
+                // dump("test");
+                // die(); 
+                $newEncodedPassword = $this->passwordEncoder->encodePassword($user, $user->getPassword());
+                $user->setPassword($newEncodedPassword);
+
+                $em->persist($user);
+                $em->flush();
+
+                return $this->redirectToRoute('admin_profile_index');
+            } else {
+                $form->addError(new FormError('Ancien mot de passe incorrect'));
+            }
+        }
+
+    	return $this->render('back/profile/editPassword.html.twig', array(
+    		'form' => $form->createView(),
+    	));
+    }
+}
