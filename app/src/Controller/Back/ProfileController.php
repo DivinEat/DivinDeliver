3c8b86692a190d82e9d1ed3daa03061019157d82
tdargent1 <tdargent1@gmail.com>
2021-02-02 12:34:37 +0100

    BUGFIX : Profile user & css

--- app/src/Controller/Back/ProfileController.php
+++ app/src/Controller/Back/ProfileController.php
@@ -4,7 +4,6 @@ namespace App\Controller\Back;
 
 use App\Form\ProfileType;
 use App\Form\EditPasswordType;
-use Symfony\Component\Form\FormError;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
