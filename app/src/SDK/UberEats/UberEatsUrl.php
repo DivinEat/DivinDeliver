5a9f832ebc176c152dd7d3ea9d8244f5c44ecd49
Taopol BERQUER <tao.berquer@gmail.com>
2021-02-08 15:08:51 +0100

    Ajout des nouvelles urls pour le SDK

--- app/src/SDK/UberEats/UberEatsUrl.php
+++ app/src/SDK/UberEats/UberEatsUrl.php
@@ -6,27 +6,27 @@ namespace App\SDK\UberEats;
 
 trait UberEatsUrl
 {
-    private string $menusUrl = 'http://host.docker.internal:8000/eats/stores/{storeID}/menus';
+    private string $menusUrl = 'http://93.90.203.240/FakeUberEats/eats/stores/{storeID}/menus';
 
-    private string $ordersUrl = 'http://host.docker.internal:8000/eats/order/{orderID}';
+    private string $ordersUrl = 'http://93.90.203.240/FakeUberEats/eats/order/{orderID}';
 
-    private string $storeActiveOrdersUrl = 'http://host.docker.internal:8000/eats/stores/{storeID}/created-orders';
+    private string $storeActiveOrdersUrl = 'http://93.90.203.240/FakeUberEats/eats/stores/{storeID}/created-orders';
 
-    private string $storeCancelOrdersUrl = 'http://host.docker.internal:8000/eats/stores/{storeID}/canceled-orders';
+    private string $storeCancelOrdersUrl = 'http://93.90.203.240/FakeUberEats/eats/stores/{storeID}/canceled-orders';
 
-    private string $acceptOrderUrl = 'http://host.docker.internal:8000/eats/orders/{orderID}/accept_pos_order';
+    private string $acceptOrderUrl = 'http://93.90.203.240/FakeUberEats/eats/orders/{orderID}/accept_pos_order';
 
-    private string $denyOrderUrl = 'http://host.docker.internal:8000/eats/orders/{orderID}/deny_pos_order';
+    private string $denyOrderUrl = 'http://93.90.203.240/FakeUberEats/eats/orders/{orderID}/deny_pos_order';
 
-    private string $cancelOrderUrl = 'http://host.docker.internal:8000/eats/orders/{orderID}/cancel';
+    private string $cancelOrderUrl = 'http://93.90.203.240/FakeUberEats/eats/orders/{orderID}/cancel';
 
     private string $storeDetailsUrl = 'https://api.uber.com/v1/eats/stores/{storeId}';
 
-    private string $allStoreUrl = 'http://host.docker.internal:8000/eats/stores';
+    private string $allStoreUrl = 'http://93.90.203.240/FakeUberEats/eats/stores';
 
-    private string $restaurantStatusUrl = 'http://host.docker.internal:8000/eats/store/{store_id}/status';
+    private string $restaurantStatusUrl = 'http://93.90.203.240/FakeUberEats/eats/store/{store_id}/status';
 
-    private string $storeHolidayHoursUrl = 'http://host.docker.internal:8000/eats/stores/{store_id}/holiday-hours';
+    private string $storeHolidayHoursUrl = 'http://93.90.203.240/FakeUberEats/eats/stores/{store_id}/holiday-hours';
 
     protected function getUrlWithParams(string $urlName, array $params =  []): ?string
     {
