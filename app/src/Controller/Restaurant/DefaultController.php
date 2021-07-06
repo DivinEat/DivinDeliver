fa612128b9625a289310e5b3b1aaf493bedcda9f
tdargent1 <tdargent1@gmail.com>
2021-07-06 20:14:08 +0200

    BUGFIX : 1.Dashboard récupération uniquement en fonction du User connécté 2.Boutons dans show order 3. Statut sur user index

--- app/src/Controller/Restaurant/DefaultController.php
+++ app/src/Controller/Restaurant/DefaultController.php
@@ -2,11 +2,6 @@
 
 namespace App\Controller\Restaurant;
 
-use App\Repository\ItemRepository;
-use App\Repository\MenuRepository;
-use App\Repository\UserRepository;
-use App\Repository\OrderRepository;
-use App\Repository\CategoryRepository;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
@@ -15,20 +10,15 @@ class DefaultController extends AbstractController
     /**
      * @Route("/", name="default_index", methods={"GET"})
      */
-    public function index(
-        ItemRepository $itemRepository,
-        UserRepository $userRepository,
-        CategoryRepository $categoryRepository,
-        MenuRepository $menuRepository,
-        OrderRepository $orderRepository
-    ) {
+    public function index()
+    {
         return $this->render('restaurant/default/index.html.twig', [
             'user' => $this->getUser(),
-            'items' => count($itemRepository->findAll()),
-            'categories' => count($categoryRepository->findAll()),
-            'users' => count($userRepository->findAll()),
-            'menus' => count($menuRepository->findAll()),
-            'orders' => count($orderRepository->findAll())
+            'items' => count($this->getUser()->getStores()->first()->getItems()),
+            'categories' => count($this->getUser()->getStores()->first()->getCategories()),
+            'users' => count($this->getUser()->getStores()->first()->getUsers()),
+            'menus' => count($this->getUser()->getStores()->first()->getMenus()),
+            'orders' => count($this->getUser()->getStores()->first()->getOrders()),
         ]);
     }
 }
