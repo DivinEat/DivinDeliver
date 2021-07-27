7cda494160f1ea28a3255b72fde3dce4ba9b046c
“remibrat” <remi.brat@outlook.fr>
2021-07-27 12:19:07 +0200

    [ADD] Password constraints

--- app/src/Form/AccountValidationType.php
+++ app/src/Form/AccountValidationType.php
@@ -3,6 +3,11 @@
 namespace App\Form;
 
 use App\Entity\User;
+use App\Validator\PasswordDigit;
+use App\Validator\PasswordLength;
+use App\Validator\PasswordSpecial;
+use App\Validator\PasswordLowercase;
+use App\Validator\PasswordUppercase;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -15,11 +20,16 @@ class AccountValidationType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('email', EmailType::class, [
-                'label' => 'account.validation.form.confirm_email',
-            ])
             ->add('password', RepeatedType::class, [
                 'type' => PasswordType::class,
+                'constraints' => [
+                    new PasswordLength(),
+                    new PasswordUppercase(),
+                    new PasswordDigit(),
+                    new PasswordSpecial(),
+                    new PasswordLowercase()
+                ],
+                'invalid_message' => 'The password fields must match',
                 'first_options' => [
                     'label' => 'account.validation.form.create_password'
                 ],
