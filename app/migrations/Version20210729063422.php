7c0e23387b145b44d6da30ea3e76cbceac9bedd4
Taopol BERQUER <tao.berquer@gmail.com>
2021-07-29 08:48:36 +0200

    user => user_account d

--- app/migrations/Version20210729063422.php
+++ app/migrations/Version20210729063422.php
@@ -22,8 +22,8 @@ final class Version20210729063422 extends AbstractMigration
         $pwd = '$argon2id$v=19$m=65536,t=4,p=1$rKB0PNfQjtI/ZBgy+Ia98Q$YvF9EZJ2pQuRFugi9BibV68gjAB8tRqAcbXYjLpnjKY';
         $this->addSql("INSERT INTO user_account (
                   id, 
-                  first_name, 
-                  last_name, 
+                  firstname, 
+                  lastname, 
                   email, 
                   password, 
                   roles, 
