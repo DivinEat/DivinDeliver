cd11e58695023bec9c71f482a4d6269ce186cd36
Taopol BERQUER <tao.berquer@gmail.com>
2021-07-29 07:22:14 +0200

    pouic pouic

--- app/src/Controller/Restaurant/Order/DefaultController.php
+++ app/src/Controller/Restaurant/Order/DefaultController.php
@@ -13,6 +13,7 @@ use Symfony\Component\HttpFoundation\Response;
 use App\SDK\Deliveroo\OrderSDK as DeliverooOrderSDK;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Security\Core\Security;
 
 /**
  * @Route("/orders", name="order_")
@@ -22,16 +23,19 @@ class DefaultController extends AbstractController
     /**
      * @Route("/", name="index", methods={"GET"})
      */
-    public function index(Request $request, OrderRepository $orderRepository): Response
+    public function index(Request $request, OrderRepository $orderRepository, Security $security): Response
     {
         $choice = $request->query->get('choice') ?? "ALL";
 
         $choice = strtoupper($choice);
 
         if ($choice !== 'ALL')
-            $orders = $orderRepository->findBy(['currentState' =>  $choice]);
+            $orders = $orderRepository->findBy([
+                'currentState' =>  $choice,
+                'storeId    ' => $security->getUser()->getStores->first()->getId()
+            ]);
         else
-            $orders = $orderRepository->findAll();
+            $orders = $security->getUser()->getStores->first()->getOrders();
 
         return $this->render('restaurant/order/index.html.twig', [
             'orders' => $orders
