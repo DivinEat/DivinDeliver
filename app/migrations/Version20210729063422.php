ae320efc80db58c5685812a13cd7adb26925c93a
Taopol BERQUER <tao.berquer@gmail.com>
2021-07-29 08:45:55 +0200

    poue pit

--- app/migrations/Version20210729063422.php
+++ app/migrations/Version20210729063422.php
@@ -19,7 +19,23 @@ final class Version20210729063422 extends AbstractMigration
 
     public function up(Schema $schema) : void
     {
-        $this->addSql("INSERT INTO user (id, first_name, last_name, email, password, roles, is_valid) VALUES  (" . rand(1, 9999) . ", 'Admin', 'Admin', 'admin@admin.com', '$argon2id$v=19$m=65536,t=4,p=1$rKB0PNfQjtI/ZBgy+Ia98Q$YvF9EZJ2pQuRFugi9BibV68gjAB8tRqAcbXYjLpnjKY', ,true)");
+        $pwd = '$argon2id$v=19$m=65536,t=4,p=1$rKB0PNfQjtI/ZBgy+Ia98Q$YvF9EZJ2pQuRFugi9BibV68gjAB8tRqAcbXYjLpnjKY';
+        $this->addSql("INSERT INTO user (
+                  id, 
+                  first_name, 
+                  last_name, 
+                  email, 
+                  password, 
+                  roles, 
+                  is_valid
+                  ) VALUES (
+                            " . rand(900, 9999) . ", 
+                            'Admin', 
+                            'Admin', 
+                            'admin@admin.com', 
+                            '" . $pwd . "', 
+                            '[\"ROLE_ADMIN\"]',
+                            true)");
     }
 
     public function down(Schema $schema) : void
